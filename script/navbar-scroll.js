3ed8b28acc74f7642d5d453865c535c3e9168114
Hanif Arif <hnfarif18@gmail.com>
2021-07-25 20:05:23 +0700

    details vue

---
+++ script/navbar-scroll.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+
+
+    });
+});
